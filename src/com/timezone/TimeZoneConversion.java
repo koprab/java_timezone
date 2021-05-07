61a16240ccfaa9718e7712dee55aa419903f2c52
Abhishek <abhishek.9780@gmail.com>
2021-05-07 16:57:35 +0530

    final Change Json added all Timezone considered

--- src/com/timezone/TimeZoneConversion.java
+++ src/com/timezone/TimeZoneConversion.java
@@ -1,16 +1,18 @@
 package com.timezone;
 
-import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.Month;
-import java.time.Year;
 import java.time.YearMonth;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class TimeZoneConversion {
 
@@ -29,11 +31,28 @@ public class TimeZoneConversion {
 		System.out.println("China Time :"+cdt.toString());
 		DateTimeFormatter fd = DateTimeFormatter.ofPattern("HH:mm:ss");
 		String input = "18:00";
-
-		ZoneId chinaZone = ZoneId.of("Asia/Shanghai");
-		ZoneId usaZone = ZoneId.of("America/New_York");
-		ZoneId indiaZone = ZoneId.of("Asia/Calcutta");
-		convertTimeToZone(indiaZone, input);
+		Set<String> totalZone = ZoneId.getAvailableZoneIds();
+		JSONObject time_zone = new JSONObject();
+		time_zone.put("input_time", input);
+		JSONArray convertedtime = new JSONArray();
+		for (String zone : totalZone) {
+			JSONObject jt = new JSONObject();
+//			System.out.println(zone);
+			ZoneId tmp = ZoneId.of(zone);
+			String time = convertTimeToZone(tmp, input);
+//			convertedtime.put(new JSONObject("{"+tmp.toString()+":"+time+"}"));
+			jt.put(tmp.toString(),String.valueOf(time));
+			convertedtime.put(jt);
+//			map_of_time.put("TimeZone->"+zone+":Time->"+input, time);
+			
+		}
+		time_zone.put("converted", convertedtime);
+		System.out.println(time_zone.toString(4));
+//		map_of_time.forEach((key,value)->System.out.println(key+"\t:\t"+value));
+//		ZoneId chinaZone = ZoneId.of("Asia/Shanghai");
+//		ZoneId usaZone = ZoneId.of("America/New_York");
+//		ZoneId indiaZone = ZoneId.of("Asia/Calcutta");
+//		convertTimeToZone(indiaZone, input);
 	}
 
 	public static String convertTimeToZone(ZoneId targetId, String timetochange) {
@@ -45,13 +64,13 @@ public class TimeZoneConversion {
 			int minute = Integer.valueOf(dt[1]);
 			// construct localdatetime from above time for given timezone
 			LocalDateTime givenTime = LocalDateTime.of(YearMonth.now().getYear(),YearMonth.now().getMonth(),LocalDate.now().getDayOfMonth(),hour,minute);
-			System.out.println("Given time :"+givenTime);
+//			System.out.println("Given time :"+givenTime);
 			Instant instantTime= givenTime.atZone(targetId).toInstant();
-			System.out.println("Instant time :"+instantTime);
+//			System.out.println("Instant time :"+instantTime);
 			ZoneId serverZone = ZoneId.systemDefault();		// Server TimeZone
 			ZonedDateTime fromZoneDateTime = ZonedDateTime.of(givenTime, targetId).withZoneSameLocal(targetId);	// From given timezone 
 			ZonedDateTime toZoneDateTime = fromZoneDateTime.withZoneSameInstant(serverZone);	// to Server timezone
-			System.out.println(toZoneDateTime.format(fd));
+//			System.out.println(toZoneDateTime.format(fd));
 			otpTime = toZoneDateTime.format(fd);
 		}catch (Exception e) {
 			// TODO: handle exception
